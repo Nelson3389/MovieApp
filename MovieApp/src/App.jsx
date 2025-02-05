ddc74f5f500bb27749ffff1fb80b5a86f3b62a98
Nelson Cruz <nelson.443@icloud.com>
2025-02-05 11:37:24 -0500

    [Feature] Make the Like buttons of the Movie titles clickable.

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -7,8 +7,8 @@ const Card = ({title}) => {
         <div className="card">
             <h2>{title}</h2>
 
-            <button onClick={() => setHasLiked(true)}>
-                Like
+            <button onClick={() => setHasLiked(!hasLiked)}>
+                {hasLiked ? '❤️' : '🤍'}
             </button>
         </div>
     )
