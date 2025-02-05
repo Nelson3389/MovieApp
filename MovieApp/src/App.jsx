778214b2a57c6df8bbf102bc2409dd64464da0d2
Nelson Cruz <nelson.443@icloud.com>
2025-02-05 11:04:36 -0500

    [Feature] Add Like button to Movie titles.

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react'
+
 const Card = ({title}) => {
+    const [hasLiked, setHasLiked] = useState(false);
+
     return (
         <div className="card">
             <h2>{title}</h2>
+
+            <button onClick={() => setHasLiked(true)}>
+                Like
+            </button>
         </div>
     )
 }
 
 const App = () => {
+
+
     return (
         <div className="card-container">
-            <Card title="Star Wars" rating ={5} isCool={true} />
-            <Card title="Avatar" />
-            <Card title="The Lion King" />
+            <Card title="Star Wars" rating={5} isCool={true}/>
+            <Card title="Avatar"/>
+            <Card title="The Lion King"/>
         </div>
     )
 }
