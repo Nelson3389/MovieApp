d284f091154637fb81c6e03185843532e4d3c25d
Nelson Cruz <nelson.443@icloud.com>
2025-02-13 16:03:47 -0500

    [Feature] Added text gradient to "Movies" in the site header.

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState, useEffect } from 'react';
-
-const Card = ({ title }) => {
-    const [count, setCount] = useState(0);
-    const [hasLiked, setHasLiked] = useState(false);
-
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`);
-    }, [hasLiked]);
-
-    return (
-        <div className="card" onClick={() => setCount(count + 1)}>
-            <h2>{title} <br/> {count || null}</h2>
-
-            <button onClick={() => setHasLiked(!hasLiked)}>
-                {hasLiked ? '❤️' : '🤍'}
-            </button>
-        </div>
-    )
-}
+import React from 'react'
+import Search from "./components/Search.jsx";
 
 const App = () => {
-
-
     return (
-        <div className="card-container">
-            <Card title="Star Wars" rating={5} isCool={true}/>
-            <Card title="Avatar"/>
-            <Card title="The Lion King"/>
-        </div>
+        <main>
+            <div className="pattern" />
+
+            <div className="wrapper">
+                <header>
+                    <img src="./hero-img.png" alt="Hero Banner" />
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy.</h1>
+                </header>
+
+                <Search />
+            </div>
+        </main>
     )
 }
 
