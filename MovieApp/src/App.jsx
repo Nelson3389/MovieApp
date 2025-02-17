6d2406a5f087abca73db0b440c46c43059a90496
Nelson Cruz <nelson.443@icloud.com>
2025-02-17 09:47:30 -0500

    [Feature] Implemented the Search component.

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Search from "./components/Search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
         <main>
             <div className="pattern" />
 
             <div className="wrapper">
                 <header>
-                    <img src="./hero-img.png" alt="Hero Banner" />
+                    <img src="/hero-img.png" alt="Hero Banner" />
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy.</h1>
                 </header>
 
-                <Search />
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                <h1 className="text-white">{searchTerm}</h1>
             </div>
         </main>
     )
